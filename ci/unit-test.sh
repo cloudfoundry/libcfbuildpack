4c5cd402cdb1b175ca9dc3e013e14f25dbb553c6
Ben Hale <bhale@pivotal.io>
2018-09-12 14:59:21 -0700

    CI Image

--- ci/unit-test.sh
+++ ci/unit-test.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-[[ -d $PWD/go-build && ! -d $HOME/.cache/go-build ]] && mkdir -p $HOME/.cache && ln -s $PWD/go-build $HOME/.cache/go-build
+GOCACHE="$PWD/go-build"
 
 cd libjavabuildpack
-go test -v
+go test
