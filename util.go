6132a29e48b7d66b0a8ff21e5d762b2794f4e86c
Ben Hale <bhale@pivotal.io>
2018-09-17 09:35:15 -0700

    Add stripcomponents to ExtractZip
    Previously, extraction of ZIP files always included the full path as embedded
    in the archive.  However, like TAR files, stripping components from the path
    can be useful.  This change adds stripcomponents functionality to the
    ExtractZip function.

--- util.go
+++ util.go
@@ -61,7 +61,7 @@ func ExtractTarGz(tarFile, destDir string, stripComponents int) error {
 }
 
 // ExtractZip extracts zipfile to destDir
-func ExtractZip(zipfile, destDir string) error {
+func ExtractZip(zipfile, destDir string, stripComponents int) error {
 	r, err := zip.OpenReader(zipfile)
 	if err != nil {
 		return err
@@ -69,7 +69,7 @@ func ExtractZip(zipfile, destDir string) error {
 	defer r.Close()
 
 	for _, f := range r.File {
-		path := filepath.Join(destDir, f.Name)
+		path := filepath.Join(append([]string{destDir}, strings.Split(f.Name, string(filepath.Separator))[stripComponents:]...)...)
 
 		rc, err := f.Open()
 		if err != nil {
