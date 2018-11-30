ff5da5a4b74c04dc926a84eb5b49389cf1d5a06e
Ben Hale <bhale@pivotal.io>
2018-11-30 09:55:04 -0800

    Be*EnvLike Matchers
    This change adds Be*EnvLike matchers to facilitate testing.
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- test/matchers.go
+++ test/matchers.go
@@ -17,6 +17,7 @@
 package test
 
 import (
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -27,13 +28,49 @@ import (
 	layersPkg "github.com/cloudfoundry/libcfbuildpack/layers"
 )
 
+// BeAppendBuildEnvLike tests that an append build env has specific content.
+func BeAppendBuildEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beBuildEnvFileLike(t, layer, fmt.Sprintf("%s.append", name), format, args...)
+}
+
+// BeAppendLaunchEnvLike tests that an append launch env has specific content.
+func BeAppendLaunchEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beLaunchEnvFileLike(t, layer, fmt.Sprintf("%s.append", name), format, args...)
+}
+
+// BeAppendSharedEnvLike tests that an append shared env has specific content.
+func BeAppendSharedEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beSharedEnvFileLike(t, layer, fmt.Sprintf("%s.append", name), format, args...)
+}
+
+// BeAppendPathBuildEnvLike tests that an append path build env has specific content.
+func BeAppendPathBuildEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beBuildEnvFileLike(t, layer, name, format, args...)
+}
+
+// BeAppendPathLaunchEnvLike tests that an append path launch env has specific content.
+func BeAppendPathLaunchEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beLaunchEnvFileLike(t, layer, name, format, args...)
+}
+
+// BeAppendPathSharedEnvLike tests that an append path shared env has specific content.
+func BeAppendPathSharedEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beSharedEnvFileLike(t, layer, name, format, args...)
+}
+
 // BeFileLike tests that a file exists, has a specific mode, and specific content.
-func BeFileLike(t *testing.T, file string, mode os.FileMode, content string) {
+func BeFileLike(t *testing.T, file string, mode os.FileMode, format string, args ...interface{}) {
 	t.Helper()
 
 	FileExists(t, file)
 	fileModeMatches(t, file, mode)
-	fileContentMatches(t, file, content)
+	fileContentMatches(t, file, format, args...)
 }
 
 // BeLayerLike tests that a layer has a specific flag configuration.
@@ -75,6 +112,30 @@ func BeLaunchMetadataLike(t *testing.T, layers layersPkg.Layers, expected layers
 	}
 }
 
+// BeOverrideBuildEnvLike tests that an override build env has specific content.
+func BeOverrideBuildEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beBuildEnvFileLike(t, layer, fmt.Sprintf("%s.override", name), format, args...)
+}
+
+// BeOverrideLaunchEnvLike tests that an override launch env has specific content.
+func BeOverrideLaunchEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beLaunchEnvFileLike(t, layer, fmt.Sprintf("%s.override", name), format, args...)
+}
+
+// BeOverrideSharedEnvLike tests that an override shared env has specific content.
+func BeOverrideSharedEnvLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	beSharedEnvFileLike(t, layer, fmt.Sprintf("%s.override", name), format, args...)
+}
+
+// BeProfileLike tests that a profile.d file has specific content.
+func BeProfileLike(t *testing.T, layer layersPkg.Layer, name string, format string, args ...interface{}) {
+	t.Helper()
+	BeFileLike(t, filepath.Join(layer.Root, "profile.d", name), 0644, format, args...)
+}
+
 // FileExists tests that a file exists
 func FileExists(t *testing.T, file string) {
 	t.Helper()
@@ -87,6 +148,26 @@ func FileExists(t *testing.T, file string) {
 
 		t.Fatal(err)
 	}
+}
+
+func beBuildEnvFileLike(t *testing.T, layer layersPkg.Layer, file string, format string, args ...interface{}) {
+	t.Helper()
+	beEnvFileLike(t, layer, filepath.Join("env.build", file), format, args...)
+}
+
+func beEnvFileLike(t *testing.T, layer layersPkg.Layer, file string, format string, args ...interface{}) {
+	t.Helper()
+	BeFileLike(t, filepath.Join(layer.Root, file), 0644, format, args...)
+}
+
+func beLaunchEnvFileLike(t *testing.T, layer layersPkg.Layer, file string, format string, args ...interface{}) {
+	t.Helper()
+	beEnvFileLike(t, layer, filepath.Join("env.launch", file), format, args...)
+}
+
+func beSharedEnvFileLike(t *testing.T, layer layersPkg.Layer, file string, format string, args ...interface{}) {
+	t.Helper()
+	beEnvFileLike(t, layer, filepath.Join("env", file), format, args...)
 }
 
 func fileModeMatches(t *testing.T, file string, mode os.FileMode) {
@@ -102,7 +183,7 @@ func fileModeMatches(t *testing.T, file string, mode os.FileMode) {
 	}
 }
 
-func fileContentMatches(t *testing.T, file string, content string) {
+func fileContentMatches(t *testing.T, file string, format string, args ...interface{}) {
 	t.Helper()
 
 	b, err := ioutil.ReadFile(file)
@@ -111,7 +192,9 @@ func fileContentMatches(t *testing.T, file string, content string) {
 	}
 
 	actual := string(b)
-	if actual != content {
-		t.Errorf("File content = %s, wanted = %s", actual, content)
+	expected := fmt.Sprintf(format, args...)
+
+	if actual != expected {
+		t.Errorf("File content = %s, wanted = %s", actual, expected)
 	}
 }
