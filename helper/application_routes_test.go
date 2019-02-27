f561491370a0c8a522312cdb8a0597c28ed06c9d
Ben Hale <bhale@pivotal.io>
2019-02-26 16:54:44 -0800

    Support ApplicationRoutes
    This change adds a helper that exposes Application Routes.
    
    [resolves #14]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

---
+++ helper/application_routes_test.go
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package helper_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/cloudfoundry/libcfbuildpack/helper"
+	"github.com/cloudfoundry/libcfbuildpack/internal"
+	"github.com/cloudfoundry/libcfbuildpack/test"
+	. "github.com/onsi/gomega"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+)
+
+func TestApplicationRoutes(t *testing.T) {
+	spec.Run(t, "ApplicationRoutes", func(t *testing.T, _ spec.G, it spec.S) {
+
+		g := NewGomegaWithT(t)
+
+		it("extracts value from CNB_APP_ROUTES", func() {
+			defer test.ReplaceEnv(t, "CNB_APP_ROUTES", `{
+  "test-type-1": {
+    "port": 1
+  },
+  "test-type-2": {
+    "uri": "test-uri"
+  }
+}`)()
+
+			g.Expect(helper.DefaultApplicationRoutes()).To(Equal(helper.ApplicationRoutes{
+				"test-type-1": {Port: 1},
+				"test-type-2": {URI: "test-uri"},
+			}))
+		})
+
+		it("returns error when CNB_APP_ROUTES not set", func() {
+			defer internal.ProtectEnv(t, "CNB_APP_ROUTES")()
+			g.Expect(os.Unsetenv("CNB_APP_ROUTES")).Should(Succeed())
+
+			_, err := helper.DefaultApplicationRoutes()
+			g.Expect(err).To(MatchError("CNB_APP_ROUTES not set"))
+		})
+	}, spec.Report(report.Terminal{}))
+}
