8df93d029d11db8aa5e91e135ccca2720f90ac7b
Ben Hale <bhale@pivotal.io>
2019-02-26 21:44:20 -0800

    Polishing
    This change adds documentation to the newly contributed type and function.
    
    [resolves #9]
    
    Signed-off-by: Ben Hale <bhale@pivotal.io>

--- helper/command_runner.go
+++ helper/command_runner.go
@@ -1,13 +1,31 @@
+/*
+ * Copyright 2018-2019 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package helper
 
 import (
-"os"
-"os/exec"
+	"os"
+	"os/exec"
 )
 
+// CommandRunner is an empty struct to hang the Run method on.
 type CommandRunner struct {
 }
 
+// Run makes CommandRunner satisfy the Runner interface.  This implementation delegates to exec.Command.
 func (r CommandRunner) Run(bin, dir string, args ...string) error {
 	cmd := exec.Command(bin, args...)
 	cmd.Dir = dir
